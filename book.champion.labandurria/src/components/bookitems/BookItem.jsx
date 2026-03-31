32953a1d08aef82d8eaa38577c24f24702a5d127
AGN200486 <vellaagustin04@gmail.com>
2026-03-31 14:28:51 -0300

    31/3/2026

--- book.champion.labandurria/src/components/bookitems/BookItem.jsx
+++ book.champion.labandurria/src/components/bookitems/BookItem.jsx
@@ -1,27 +1,31 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const BookItem = ({ book }) => {
-  return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={book.imageUrl} />
-        <Card.Body>
-          <Card.Title>{book.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {book.author}
-          </Card.Subtitle>
-          <Card.Text>
-            {book.rating}
-            {book.pageCount}
-            {book.summary}
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </>
-  );
+const BookItem = ({ title, author, rating, pageCount, imageUrl, available }) => {
+    const [bookTitle, setBookTitle] = useState(title);
+
+    const handleClick = () => {
+        setBookTitle("Actualizado!");
+    };
+
+    return (
+        <Card className="mx-3" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={imageUrl} />
+            <Card.Body>
+                <Card.Title>{bookTitle}</Card.Title>
+                <Card.Subtitle>{author}</Card.Subtitle>
+                <div>
+                    <div>{rating} estrellas</div>
+                    <p>{pageCount} páginas</p>
+                    <p>{available ? "Disponible" : "Reservado"}</p>
+                </div>
+                <Button variant="dark" onClick={handleClick}>
+                    Cambiar titulo
+                </Button>
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default BookItem;
---
+++ book.champion.labandurria/src/components/books/Books.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import BookItem from '../bookitems/BookItem';
+
+const Books = ({ books }) => {
+    return (
+        <div className="d-flex justify-content-center flex-wrap">
+            {books.map((book) => (
+                <BookItem
+                    key={book.id}
+                    title={book.title}
+                    author={book.author}
+                    rating={book.rating}
+                    pageCount={book.pageCount}
+                    imageUrl={book.imageUrl}
+                    available={book.available}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Books;
