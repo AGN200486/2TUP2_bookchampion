94fdfcd6c5519150d9bd0153d7dab8bdea923300
AGN200486 <vellaagustin04@gmail.com>
2026-04-20 17:03:39 -0300

    20/4

--- book.champion.labandurria_api/src/index.js
+++ book.champion.labandurria_api/src/index.js
@@ -1,22 +1,16 @@
 import express from "express";
 import { PORT } from "./config.js";
-import bookRoutes from "./routes/books.routes.js"
+import bookRoutes from "./routes/books.routes.js";
 import { sequelize } from "../db.js";
-
 
 const app = express();
 
-try{
-    app.use(bookRoutes);
-    app.listen(PORT);
+try {
+  app.use(bookRoutes);
+  app.listen(PORT);
 
-    await sequelize.authenticate();
-    console.log(`Server listening on port ${PORT}`);
-} catch (error){
-    console.log("there was an error on initializacion");
+  await sequelize.authenticate();
+  console.log(`Server listening on port ${PORT}`);
+} catch (error) {
+  console.log("there was an error on initializacion", error);
 }
-
-app.use(bookRoutes);
-
-app.listen(PORT);
-console.log(`server is running on port ${PORT}`);
--- book.champion.labandurria_api/src/routes/books.routes.js
+++ book.champion.labandurria_api/src/routes/books.routes.js
@@ -3,11 +3,11 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/books", (req, res) => {
-    res.send("Obteniendo libros")
+  res.send("Obteniendo libros");
 });
 
 router.get("books/:id", (req, res) => {
-    const {id} = req.params;
-    res.send(`Obteniendo libro con id: ${id}`)
+  const { id } = req.params;
+  res.send(`Obteniendo libro con id: ${id}`);
 });
 export default router;
